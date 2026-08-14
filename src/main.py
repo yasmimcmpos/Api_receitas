362f9aeaab4d847f148dbc1af1dd3c55e47e0861
Yasmim <yasmimfer987@outlook.com>
2026-08-13 23:17:02 -0300

    testando manipular resposta json

--- src/main.py
+++ src/main.py
@@ -10,13 +10,20 @@ url='https://api.spoonacular.com/recipes/findByIngredients'
 
 parametros = {
     'apiKey':apiKey,
-    'ingredients':'apples',
-    'number':2,
+    'ingredients':'flour',
+    'number':1,
 }
 
 resposta = requests.get(url, params=parametros)
+
 if resposta.status_code == 200:
-    dados = resposta.json()
-    print(dados)
+    dados_lista = resposta.json()
+    for dicionario in dados_lista:
+        for chave in dicionario.keys():
+            print(f'{chave} = {dicionario[chave]}')
+        #print(dicionario.items())
+        #print(dicionario["title"])
+        #print(dicionario["image"])
 else:
     print("Erro:", resposta.status_code)
+
---
+++ testes.py
@@ -0,0 +1,13 @@
+dados = [{"id": 673463, "title": "Slow Cooker Apple Pork Tenderloin"}]
+
+#Consegui ler a lista:
+
+for info in dados:
+    for chave, valor in info["id","title"]:
+        print(dados)
+
+
+
+#quando eu acesso dados[0] - eu chego no meu dicionário
+#quando é só [dados] - eu chego só na minha lista
+#Preciso ler o dicionário de dentro da lista
